908c6e45c1d45433059b409edbca3dc0d7055d91
antsimch <anthonysimch@gmail.com>
2023-06-21 13:51:36 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/paf22_workshop/Paf22WorkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf22_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paf22WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paf22WorkshopApplication.class, args);
+	}
+
+}
